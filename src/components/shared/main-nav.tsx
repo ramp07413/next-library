b1db6e0f229b2eb0b394ab6f3917be33eaf4c30a
r t <ramp07413@gmail.com>
2025-10-01 05:45:00 +0000

    add padding top of search nav

--- src/components/shared/main-nav.tsx
+++ src/components/shared/main-nav.tsx
@@ -11,7 +11,7 @@ interface MainNavProps {
 
 export default function MainNav({ role }: MainNavProps) {
   return (
-    <header className="sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+    <header className="sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 pt-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
       <SidebarTrigger className="sm:hidden" />
       <div className="relative ml-auto flex-1 md:grow-0">
         <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
