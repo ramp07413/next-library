26d2f8f06c60cb51fd4c6e58b0da01c8c11c3674
r t <ramp07413@gmail.com>
2025-10-01 05:21:57 +0000

    Try fixing this error: `Runtime Error: ReferenceError: cn is not defined

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -19,6 +19,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Progress } from "@/components/ui/progress";
+import { cn } from "@/lib/utils";
 
 export default function CompanyDashboard() {
   return (
