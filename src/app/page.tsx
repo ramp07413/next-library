b977ee294b193f9df0d076f437e251cc1bb72514
r t <ramp07413@gmail.com>
2025-10-06 11:59:31 +0000

    navbar color apply to footer in dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -139,7 +139,7 @@ export default function Home() {
         </section>
         <Separator />
       </main>
-      <footer className="bg-zinc-800">
+      <footer className="bg-zinc-800 dark:bg-background">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
             <div className="md:col-span-2">
