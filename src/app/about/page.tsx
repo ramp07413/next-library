f5ea21c1e184f7f81d0a97d057a3039920fc0589
r t <ramp07413@gmail.com>
2025-10-08 10:46:55 +0000

    undo the change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -162,7 +162,7 @@ export default function AboutUsPage() {
       </main>
       <footer className="bg-zinc-900">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12 border-t border-zinc-800">
-          <p className="text-left text-sm text-white">&copy; {new Date().getFullYear()} LibMan Platform. All rights reserved.</p>
+          <p className="text-center sm:text-left text-sm text-white">&copy; {new Date().getFullYear()} LibMan Platform. All rights reserved.</p>
         </div>
       </footer>
     </div>
